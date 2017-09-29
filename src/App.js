18440c5208fef11d460fbcea7c95b2d51775ef93
kyle <the.kyle.maune@gmail.com>
2017-09-29 15:21:52 -0700

    cleaned up app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
-import IdeasContainer from './components/IdeasContainer';
+import React, { Component } from 'react'
+import './App.css'
+import IdeasContainer from './components/IdeasContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <h1>Idea Board</h1>
-        </header>
+        </div>
         <IdeasContainer />
       </div>
     );
   }
 }
 
-export default App;
+export default App
