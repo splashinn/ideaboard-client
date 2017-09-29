c90244096aaacf6c71ea42038d17b95d7d3e505a
kyle <the.kyle.maune@gmail.com>
2017-09-29 14:38:52 -0700

    Working on update information

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -14,10 +14,27 @@ class IdeaForm extends Component {
     this.setState({[e.target.name]: e.target.value})
   }
 
+  handleBlur = () => {
+    const idea = {
+      title: this.state.title,
+      body: this.state.body
+    }
+
+    axios.put(
+      `http://localhost:3001/api/v1/ideas/${this.props.idea.id}`,
+      {
+        idea: idea
+      })
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div className="tile">
-        <form>
+        <form onBlur={this.handleBlur} >
           <input className='input' type="text"
             name="title" placeholder='Enter a Title' value={this.state.title} onChange={this.handleInput} />
           <textarea className='input' name="body"
