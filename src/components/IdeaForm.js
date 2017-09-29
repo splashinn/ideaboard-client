5fae52eda7dd8635b2f8641afc67d335b73286ea
kyle <the.kyle.maune@gmail.com>
2017-09-29 15:22:28 -0700

    finished up idea form

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -2,31 +2,28 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 class IdeaForm extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
+	constructor(props) {
+		super(props)
+		this.state = {
       title: this.props.idea.title,
       body: this.props.idea.body
-    }
-  }
+		}
+	}
 
   handleInput = (e) => {
+    this.props.resetNotification()
     this.setState({[e.target.name]: e.target.value})
   }
 
   handleBlur = () => {
-    const idea = {
-      title: this.state.title,
-      body: this.state.body
-    }
-
+    const idea = {title: this.state.title, body: this.state.body }
     axios.put(
       `http://localhost:3001/api/v1/ideas/${this.props.idea.id}`,
-      {
-        idea: idea
-      })
+      {idea: idea}
+      )
     .then(response => {
       console.log(response)
+      this.props.updateIdea(response.data)
     })
     .catch(error => console.log(error))
   }
@@ -34,12 +31,13 @@ class IdeaForm extends Component {
   render() {
     return (
       <div className="tile">
-        <form onBlur={this.handleBlur} >
-          <input className='input' type="text"
-            name="title" placeholder='Enter a Title' value={this.state.title} onChange={this.handleInput} />
-          <textarea className='input' name="body"
-            placeholder="What's your idea?" value={this.state.body} onChange={this.handleInput}></textarea>
-        </form>
+      	<form onBlur={this.handleBlur} >
+					<input className='input' type="text" name="title" placeholder='Enter a Title'
+            value={this.state.title} onChange={this.handleInput}
+            ref={this.props.titleRef} />
+					<textarea className='input' name="body" placeholder='Describe your idea'
+            value={this.state.body} onChange={this.handleInput}></textarea>
+      	</form>
       </div>
     );
   }
