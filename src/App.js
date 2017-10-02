1444be8e54d518792ba9bcca0a02b957634e33cd
kyle <the.kyle.maune@gmail.com>
2017-10-02 11:13:19 -0700

    added font and some padding

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>Idea Board</h1>
+          <h1>CASCO Idea Board</h1>
         </div>
         <IdeasContainer />
       </div>
